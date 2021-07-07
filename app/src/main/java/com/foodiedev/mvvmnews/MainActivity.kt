bc92eec76f8543451d3de445b31234ebd7de9f10
sriharsha1507 <sriharsha1507@gmail.com>
2021-07-06 22:24:39 -0500

    Work on navigation

--- app/src/main/java/com/foodiedev/mvvmnews/MainActivity.kt
+++ app/src/main/java/com/foodiedev/mvvmnews/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.foodiedev.mvvmnews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.foodiedev.mvvmnews.databinding.ActivityMainBinding
+import com.foodiedev.mvvmnews.features.bookmarks.BookmarksFragment
+import com.foodiedev.mvvmnews.features.breakingnews.BreakingNewsFragment
+import com.foodiedev.mvvmnews.features.searchnews.SearchNewsFragment
+
+private const val TAG_BREAKING_NEWS_FRAGMENT = "TAG_BREAKING_NEWS_FRAGMENT"
+private const val TAG_SEARCH_NEWS_FRAGMENT = "TAG_SEARCH_NEWS_FRAGMENT"
+private const val TAG_BOOKMARKS_FRAGMENT = "TAG_BOOKMARKS_FRAGMENT"
+private const val KEY_SELECTED_INDEX = "KEY_SELECTED_INDEX"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var bookmarksFragment: BookmarksFragment
+    private lateinit var searchNewsFragment: SearchNewsFragment
+    private lateinit var breakingNewsFragment: BreakingNewsFragment
+
+    private val fragments: Array<Fragment>
+        get() = arrayOf(breakingNewsFragment, searchNewsFragment, bookmarksFragment)
+
+    private var selectedIndex = 0
+
+    private val selectedFragment get() = fragments[selectedIndex]
+
+    private fun selectFragment(selectedFragment: Fragment) {
+        var transaction = supportFragmentManager.beginTransaction()
+        fragments.forEachIndexed { index, fragment ->
+            if (selectedFragment == fragment) {
+                selectedIndex = index
+                transaction = transaction.attach(fragment)
+            } else {
+                transaction.detach(fragment)
+            }
+        }
+        transaction.commit()
+
+        title = when (selectedFragment) {
+            is BreakingNewsFragment -> getString(R.string.title_breaking_news)
+            is BookmarksFragment -> getString(R.string.title_bookmarks)
+            is SearchNewsFragment -> getString(R.string.title_search_news)
+            else -> ""
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            breakingNewsFragment = BreakingNewsFragment()
+            bookmarksFragment = BookmarksFragment()
+            searchNewsFragment = SearchNewsFragment()
+
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, breakingNewsFragment, TAG_BREAKING_NEWS_FRAGMENT)
+                .add(R.id.fragment_container, searchNewsFragment, TAG_SEARCH_NEWS_FRAGMENT)
+                .add(R.id.fragment_container, bookmarksFragment, TAG_BOOKMARKS_FRAGMENT)
+                .commit()
+        } else {
+            breakingNewsFragment =
+                supportFragmentManager.findFragmentByTag(TAG_BREAKING_NEWS_FRAGMENT) as BreakingNewsFragment
+            searchNewsFragment =
+                supportFragmentManager.findFragmentByTag(TAG_SEARCH_NEWS_FRAGMENT) as SearchNewsFragment
+            bookmarksFragment =
+                supportFragmentManager.findFragmentByTag(TAG_BOOKMARKS_FRAGMENT) as BookmarksFragment
+
+            selectedIndex = savedInstanceState.getInt(KEY_SELECTED_INDEX, 0)
+        }
+
+        selectFragment(selectedFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.nav_breaking -> breakingNewsFragment
+                R.id.nav_search -> searchNewsFragment
+                R.id.nav_bookmarks -> bookmarksFragment
+                else -> throw IllegalArgumentException("Unexpected itemId")
+            }
+
+            selectFragment(fragment)
+            true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (selectedIndex != 0) {
+            binding.bottomNav.selectedItemId = R.id.nav_breaking
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_SELECTED_INDEX, selectedIndex)
     }
 }
